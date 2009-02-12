3b11fe4eebc3d3a8521e15089aafb5551b0c0dd0
Andrea Franz <andrea@gravityblast.com>
2009-02-13 00:46:48 +0100

    renamed generators

--- rails_generators/theme/theme_generator.rb
+++ rails_generators/theme/theme_generator.rb
@@ -1,71 +1,32 @@
-class ThemeGenerator < Rails::Generator::NamedBase
+class ThemeGenerator < Rails::Generator::Base
   
-  default_options :layout => false,
-                  :will_paginate => false
-  
-  attr_reader :controller_routing_path, 
-              :singular_controller_routing_path,
-              :columns,
-              :model_name,
-              :plural_model_name,
-              :resource_name,
-              :plural_resource_name
-  
+  default_options :theme => :default
+    
   def initialize(runtime_args, runtime_options = {})
     super
-    @controller_path  = runtime_args.shift
-    @model_name       = runtime_args.shift   
+    @name = @args.first || 'application'
   end
   
   def manifest
-    @controller_routing_path          = @table_name    
-    @singular_controller_routing_path = @table_name.singularize    
-    base_name, @controller_class_path, @controller_file_path, @controller_class_nesting, @controller_class_nesting_depth = extract_modules(@controller_path)    
-    @model_name = base_name.singularize unless @model_name
-    
-    # Post
-    @model_name           = @model_name.camelize 
-    # Posts
-    @plural_model_name    = @model_name.pluralize
-    # post 
-    @resource_name        = @model_name.downcase 
-    # posts
-    @plural_resource_name = @resource_name.pluralize 
-            
-    @columns = get_columns    
-          
     record do |m|      
-      m.directory(File.join('app/views', @controller_file_path))                        
-      m.template('view_tables.html.erb',  File.join("app/views", @controller_file_path, "index.html.erb"))
-      m.template('view_new.html.erb',  File.join("app/views", @controller_file_path, "new.html.erb"))
-      m.template('view_edit.html.erb',  File.join("app/views", @controller_file_path, "edit.html.erb"))
-      m.template('view_sidebar.html.erb',  File.join("app/views", @controller_file_path, "_sidebar.html.erb"))
-      
-      if options[:layout]
-        m.gsub_file(File.join("app/views/layouts", "#{options[:layout]}.html.erb"), /\<div\s+id=\"main-navigation\">.*\<\/ul\>/mi) do |match|
-          match.gsub!(/\<\/ul\>/, "")
-          %|#{match} <li><a href="<%= #{plural_resource_name}_path %>">#{plural_model_name}</a></li></ul>|
-        end
-      end
+      m.directory("app/views/layouts")
+      m.directory("public/stylesheets/themes/#{options[:theme]}/")
+      m.template("view_layout.html.erb", File.join("app/views/layouts", "#{@name}.html.erb"))
+      m.template("../../../stylesheets/base.css",  File.join("public/stylesheets", "web_app_theme.css"))
+      m.template("../../../stylesheets/themes/#{options[:theme]}/style.css",  File.join("public/stylesheets/themes/#{options[:theme]}", "style.css"))      
     end
   end
   
+  def banner
+    "Usage: #{$0} theme [layout_name] [options]"
+  end
+
 protected
 
-  def get_columns
-    excluded_column_names = %w[id created_at updated_at]
-    Kernel.const_get(@model_name).columns.reject{|c| excluded_column_names.include?(c.name) }.collect{|c| Rails::Generator::GeneratedAttribute.new(c.name, c.type)}
-  end
-  
-  def banner
-    "Usage: #{$0} theme ControllerPath [ModelName] [options]"
-  end
-  
   def add_options!(opt)
     opt.separator ''
     opt.separator 'Options:'
-    opt.on("-l", "--layout=layout", String, "Add menu link") { |v| options[:layout] = v }    
-    opt.on("-w", "--with_will_paginate", "Add pagination using will_paginate") { |v| options[:will_paginate] = true }
+    opt.on("-m", "--theme=theme", String, "Specify the theme") { |v| options[:theme] = v }
   end
   
 end
---
+++ rails_generators/themed/themed_generator.rb
@@ -0,0 +1,71 @@
+class ThemedGenerator < Rails::Generator::NamedBase
+  
+  default_options :layout => false,
+                  :will_paginate => false
+  
+  attr_reader :controller_routing_path, 
+              :singular_controller_routing_path,
+              :columns,
+              :model_name,
+              :plural_model_name,
+              :resource_name,
+              :plural_resource_name
+  
+  def initialize(runtime_args, runtime_options = {})
+    super
+    @controller_path  = runtime_args.shift
+    @model_name       = runtime_args.shift   
+  end
+  
+  def manifest
+    @controller_routing_path          = @table_name    
+    @singular_controller_routing_path = @table_name.singularize    
+    base_name, @controller_class_path, @controller_file_path, @controller_class_nesting, @controller_class_nesting_depth = extract_modules(@controller_path)    
+    @model_name = base_name.singularize unless @model_name
+    
+    # Post
+    @model_name           = @model_name.camelize 
+    # Posts
+    @plural_model_name    = @model_name.pluralize
+    # post 
+    @resource_name        = @model_name.downcase 
+    # posts
+    @plural_resource_name = @resource_name.pluralize 
+            
+    @columns = get_columns    
+          
+    record do |m|      
+      m.directory(File.join('app/views', @controller_file_path))                        
+      m.template('view_tables.html.erb',  File.join("app/views", @controller_file_path, "index.html.erb"))
+      m.template('view_new.html.erb',  File.join("app/views", @controller_file_path, "new.html.erb"))
+      m.template('view_edit.html.erb',  File.join("app/views", @controller_file_path, "edit.html.erb"))
+      m.template('view_sidebar.html.erb',  File.join("app/views", @controller_file_path, "_sidebar.html.erb"))
+      
+      if options[:layout]
+        m.gsub_file(File.join("app/views/layouts", "#{options[:layout]}.html.erb"), /\<div\s+id=\"main-navigation\">.*\<\/ul\>/mi) do |match|
+          match.gsub!(/\<\/ul\>/, "")
+          %|#{match} <li><a href="<%= #{plural_resource_name}_path %>">#{plural_model_name}</a></li></ul>|
+        end
+      end
+    end
+  end
+  
+protected
+
+  def get_columns
+    excluded_column_names = %w[id created_at updated_at]
+    Kernel.const_get(@model_name).columns.reject{|c| excluded_column_names.include?(c.name) }.collect{|c| Rails::Generator::GeneratedAttribute.new(c.name, c.type)}
+  end
+  
+  def banner
+    "Usage: #{$0} themed ControllerPath [ModelName] [options]"
+  end
+  
+  def add_options!(opt)
+    opt.separator ''
+    opt.separator 'Options:'
+    opt.on("-l", "--layout=layout", String, "Add menu link") { |v| options[:layout] = v }    
+    opt.on("-w", "--with_will_paginate", "Add pagination using will_paginate") { |v| options[:will_paginate] = true }
+  end
+  
+end
