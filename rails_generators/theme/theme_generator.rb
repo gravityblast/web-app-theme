86e10e2355410b01633071ae63eeb30f0eb22e7e
Andrea Franz <andrea@gravityblast.com>
2009-02-13 22:56:55 +0100

    added restful_authentication themer

--- rails_generators/theme/theme_generator.rb
+++ rails_generators/theme/theme_generator.rb
@@ -1,6 +1,7 @@
 class ThemeGenerator < Rails::Generator::Base
   
-  default_options :theme => :default,
+  default_options :layout_type => :administration,
+                  :theme => :default,
                   :no_layout => false
     
   def initialize(runtime_args, runtime_options = {})
@@ -12,7 +13,7 @@ class ThemeGenerator < Rails::Generator::Base
     record do |m|            
       m.directory("app/views/layouts")
       m.directory("public/stylesheets/themes/#{options[:theme]}/")      
-      m.template("view_layout.html.erb", File.join("app/views/layouts", "#{@name}.html.erb")) unless options[:no_layout]
+      m.template("view_layout_#{options[:layout_type]}.html.erb", File.join("app/views/layouts", "#{@name}.html.erb")) unless options[:no_layout]
       m.template("../../../stylesheets/base.css",  File.join("public/stylesheets", "web_app_theme.css"))
       m.template("../../../stylesheets/themes/#{options[:theme]}/style.css",  File.join("public/stylesheets/themes/#{options[:theme]}", "style.css"))      
     end
@@ -27,7 +28,8 @@ protected
   def add_options!(opt)
     opt.separator ''
     opt.separator 'Options:'
-    opt.on("-m", "--theme=theme", String, "Specify the theme") { |v| options[:theme] = v }
+    opt.on("--type=layout_type", String, "Specify the layout type") { |v| options[:layout_type] = v }
+    opt.on("--theme=theme", String, "Specify the theme") { |v| options[:theme] = v }
     opt.on("--no-layout", "Don't create layout") { |v| options[:no_layout] = true }
   end
   
--- rails_generators/themed/themed_generator.rb
+++ rails_generators/themed/themed_generator.rb
@@ -1,6 +1,7 @@
 class ThemedGenerator < Rails::Generator::NamedBase
   
-  default_options :layout => false,
+  default_options :themed_type => :crud,
+                  :layout => false,
                   :will_paginate => false
   
   attr_reader :controller_routing_path, 
@@ -30,29 +31,41 @@ class ThemedGenerator < Rails::Generator::NamedBase
     # post 
     @resource_name        = @model_name.downcase 
     # posts
-    @plural_resource_name = @resource_name.pluralize 
-           
-    @columns = get_columns    
-          
-    record do |m|      
-      m.directory(File.join('app/views', @controller_file_path))                        
-      m.template('view_tables.html.erb',  File.join("app/views", @controller_file_path, "index.html.erb"))
-      m.template('view_new.html.erb',  File.join("app/views", @controller_file_path, "new.html.erb"))
-      m.template('view_edit.html.erb',  File.join("app/views", @controller_file_path, "edit.html.erb"))
-      m.template('view_show.html.erb',  File.join("app/views", @controller_file_path, "show.html.erb"))
-      m.template('view_sidebar.html.erb',  File.join("app/views", @controller_file_path, "_sidebar.html.erb"))
-      
-      if options[:layout]
-        m.gsub_file(File.join("app/views/layouts", "#{options[:layout]}.html.erb"), /\<div\s+id=\"main-navigation\">.*\<\/ul\>/mi) do |match|
-          match.gsub!(/\<\/ul\>/, "")
-          %|#{match} <li class="<%= controller.controller_path == '#{@controller_file_path}' ? 'active' : '' %>"><a href="<%= #{controller_routing_path}_path %>">#{plural_model_name}</a></li></ul>|
-        end
-      end
+    @plural_resource_name = @resource_name.pluralize                
+    
+    manifest_method = "manifest_for_#{options[:themed_type]}"    
+    record do |m|
+      send(manifest_method, m) if respond_to?(manifest_method)
     end
   end
   
 protected
-
+  
+  def manifest_for_crud(m)
+    @columns = get_columns
+    m.directory(File.join('app/views', @controller_file_path))                        
+    m.template('view_tables.html.erb',  File.join("app/views", @controller_file_path, "index.html.erb"))
+    m.template('view_new.html.erb',     File.join("app/views", @controller_file_path, "new.html.erb"))
+    m.template('view_edit.html.erb',    File.join("app/views", @controller_file_path, "edit.html.erb"))
+    m.template('view_show.html.erb',    File.join("app/views", @controller_file_path, "show.html.erb"))
+    m.template('view_sidebar.html.erb', File.join("app/views", @controller_file_path, "_sidebar.html.erb"))
+    
+    if options[:layout]
+      m.gsub_file(File.join("app/views/layouts", "#{options[:layout]}.html.erb"), /\<div\s+id=\"main-navigation\">.*\<\/ul\>/mi) do |match|
+        match.gsub!(/\<\/ul\>/, "")
+        %|#{match} <li class="<%= controller.controller_path == '#{@controller_file_path}' ? 'active' : '' %>"><a href="<%= #{controller_routing_path}_path %>">#{plural_model_name}</a></li></ul>|
+      end
+    end
+  end
+  
+  def manifest_for_restful_authentication(m)
+    signup_controller_path  = @controller_file_path
+    signin_controller_path  = @model_name # just here I use the second argument as a controller path
+    @resource_name          = @controller_path.singularize
+    m.template('view_signup.html.erb',  File.join("app/views", signup_controller_path, "new.html.erb"))
+    m.template('view_signin.html.erb',  File.join("app/views", signin_controller_path, "new.html.erb"))
+  end
+  
   def get_columns
     excluded_column_names = %w[id created_at updated_at]
     Kernel.const_get(@model_name).columns.reject{|c| excluded_column_names.include?(c.name) }.collect{|c| Rails::Generator::GeneratedAttribute.new(c.name, c.type)}
@@ -65,8 +78,9 @@ protected
   def add_options!(opt)
     opt.separator ''
     opt.separator 'Options:'
-    opt.on("-l", "--layout=layout", String, "Add menu link") { |v| options[:layout] = v }    
-    opt.on("-w", "--with_will_paginate", "Add pagination using will_paginate") { |v| options[:will_paginate] = true }
+    opt.on("--type=themed_type", String, "") { |v| options[:themed_type] = v }    
+    opt.on("--layout=layout", String, "Add menu link") { |v| options[:layout] = v }    
+    opt.on("--with_will_paginate", "Add pagination using will_paginate") { |v| options[:will_paginate] = true }
   end
   
 end
