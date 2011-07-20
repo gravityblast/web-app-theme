15ab4e54a9e05622d6018c4de0971aaf82c05ea3
nathanvda <nathan@dixis.com>
2011-07-20 23:26:58 +0200

    Simplified a bit.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,22 +3,12 @@ ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rspec/rails"
+#require 'rspec/core'
 
 require "rails/generators"
 require "rails/generators/base"
 
 
-
-# Run any available migration
-ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
-
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # == Mock Framework
@@ -30,16 +20,4 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
-  config.before(:all) do
-    seed_file = File.join(Rails.root, 'db', 'seeds.rb')
-    load(seed_file)
-  end
 end
