8cd999b545e0ad1621e43a19f836015cd617c272
Tiago Scolari <tscolari@gmail.com>
2011-08-27 00:56:20 -0300

    Dummy application running without any complication

--- lib/web_app_theme/version.rb
+++ lib/web_app_theme/version.rb
@@ -1,3 +1,3 @@
 module WebAppTheme
-  VERSION = "0.0.1"
+  VERSION = "0.8.0"
 end
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+# require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+ require "sprockets/railtie"
+ require "rails/test_unit/railtie"
 
 Bundler.require
 require "web_app_theme"
