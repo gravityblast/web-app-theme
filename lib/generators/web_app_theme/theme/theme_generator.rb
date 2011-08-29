a06017ed289e41493ebdd3c66c19b0a96c03fd23
Jonhnny Weslley <jw@jonhnnyweslley.net>
2011-08-29 17:02:15 -0300

    refactoring generators. for now, the theme generator works only on Rails 3.1 or greater

--- lib/generators/web_app_theme/theme/theme_generator.rb
+++ lib/generators/web_app_theme/theme/theme_generator.rb
@@ -1,49 +1,34 @@
 module WebAppTheme
   class ThemeGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
-    
+
     argument :layout_name, :type => :string, :default => 'application'
-    
+
     class_option :theme,        :type => :string,   :default => :default,   :desc => 'Specify the layout theme'
     class_option :app_name,     :type => :string,   :default => 'Web App',  :desc => 'Specify the application name'
     class_option :engine,       :type => :string,   :default => 'erb',      :desc => 'Specify the template engine'
     class_option :no_layout,    :type => :boolean,  :default => false,      :desc => 'Use this option if you want to generate only stylesheets'
     class_option :layout_type,  :type => :string,   :default => 'admin',    :desc => 'Layout type, admin or sign'
-    
+
     def copy_layout
-      return if options.no_layout      
+      return if options.no_layout
       admin_layout_name = options.layout_type == 'sign' ? "layout_sign.html.erb" : "layout_admin.html.erb"
       case options.engine
       when 'erb'
         template  admin_layout_name, "app/views/layouts/#{layout_name.underscore}.html.erb"
       when 'haml'
-        generate_haml_layout(admin_layout_name)        
-      end                  
+        generate_haml_layout(admin_layout_name)
+      end
     end
 
-    def copy_base_stylesheets
-      copy_file "#{stylesheets_path}/base.css",     "public/stylesheets/web-app-theme/base.css"
-      copy_file "#{stylesheets_path}/override.css", "public/stylesheets/web-app-theme/override.css"
+    def declare_stylesheet_theme
+      inject_into_file 'app/assets/stylesheets/application.css',
+        "\n *= require 'web-app-theme'\n *= require 'web-app-theme/#{options.theme}'",
+        :after => ' *= require_self'
     end
-    
-    def copy_theme_stylesheets
-      directory "#{stylesheets_path}/themes/#{options.theme}", "public/stylesheets/web-app-theme/themes/#{options.theme}"
-    end
-    
-    def copy_images
-      directory "#{images_path}", "public/images/web-app-theme"
-    end
-    
+
   protected
-  
-    def stylesheets_path
-      "../../../../../stylesheets"
-    end
-    
-    def images_path
-      "../../../../../images"
-    end
-    
+
     def generate_haml_layout(admin_layout_name)
       require 'haml'
       Dir.mktmpdir('web-app-theme-haml') do |haml_root|
